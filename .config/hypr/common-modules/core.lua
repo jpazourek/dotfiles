f66b70ec738611bc42dd3a3fcb03cb4cbdb8d603
Jan Pazourek <github@alias.janpazourek.com>
2026-07-13 20:38:55 +0200

    hypr: added bind to change between us and cz keyboard

--- .config/hypr/common-modules/core.lua
+++ .config/hypr/common-modules/core.lua
@@ -18,10 +18,10 @@ hl.config({
 	},
 
 	input = {
-		kb_layout = "us",
+		kb_layout = "us,cz",
 		kb_variant = "",
 		kb_model = "",
-		kb_options = "",
+		kb_options = "grp:alt_shift_toggle",
 		kb_rules = "",
 
 		follow_mouse = 1,
